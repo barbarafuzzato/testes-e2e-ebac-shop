4da3a2bd4872ac29a760a5e219f9413171ae2ad4
Bárbara <barbarafuzzato@gmail.com>
2022-01-29 23:23:34 -0300

    Alterações Tarefa E2E - Módulo 12

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,21 +1,121 @@
 /// <reference types="cypress" />
+let dadosLogin
+const perfil = require('../fixtures/perfil.json')
+import EnderecoPage from '../support/page_objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
-context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-    /*  Como cliente 
-        Quero acessar a Loja EBAC 
-        Para fazer um pedido de 4 produtos 
-        Fazendo a escolha dos produtos
-        Adicionando ao carrinho
-        Preenchendo todas opções no checkout
-        E validando minha compra ao final */
+context('Funcionalidade Login', () => {
+    before(() => {
+        cy.fixture('perfil').then(perfil => {
+            dadosLogin = perfil
+        })
+    });
 
     beforeEach(() => {
-        cy.visit('/')
+        cy.visit('minha-conta')
     });
 
-    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
+    afterEach(() => {
+        cy.screenshot()
     });
 
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
+        cy.get('#username').type('aluno_ebac@teste')
+        cy.get('#password').type('teste@teste')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain', 'Erro: O usuário aluno_ebac@teste não')
+    })
 
-})
+    it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
+    })
+
+    it('Deve fazer login com sucesso', () => {
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.page-title').should('contain', 'Minha conta')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac (não é aluno_ebac? Sair)')
+    })
+
+    it('Deve fazer login com sucesso - Usando arquivo de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.page-title').should('contain', 'Minha conta')
+    });
+
+    it('Deve fazer login com sucesso - Usando fixture', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha, { log: false })
+            cy.get('.woocommerce-form > .button').click()
+        })
+
+    });
+
+});
+
+describe('Funcionalidade Página de Produtos + Checkout', () => {
+
+    beforeEach(() => {
+        cy.visit('minha-conta')
+        cy.fixture('perfil').then(dados =>{
+            cy.login(dados.usuario, dados.senha)
+        cy.visit('produtos')
+        })
+
+    });
+
+    it('Deve adicionar um produto ao carrinho', () => {
+        var quantidade = 1
+
+        cy.get('[class="product-block grid"]')
+            .contains('Ariel Roll Sleeve Sweatshirt').click()
+        cy.get('.button-variable-item-XL').click()
+        cy.get('.button-variable-item-Purple').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain' , quantidade)
+        cy.get('.woocommerce-message').should('contain' , '“Ariel Roll Sleeve Sweatshirt” foi adicionado no seu carrinho.')
+
+    });
+
+    it('Deve adicionar produtos ao carrinho - Usando comando customizado', () => {
+        cy.addProdutos('Abominable Hoodie', 'L', 'Blue', 1)
+
+    });
+
+    it('Deve adicionar produtos ao carrinho - Usando comando customizado', () => {
+        cy.addProdutos('Aether Gym Pant', '36', 'Brown', 1)
+
+    });
+
+    it('Deve adicionar produtos ao carrinho - Usando comando customizado', () => {
+        cy.addProdutos('Arcadio Gym Short', '33', 'Black', 1)
+        cy.get('.woocommerce-message > .button').click()
+
+    });
+
+    it('Deve fazer Checkout com sucesso - Usando arquivo  de dados', () => {
+        cy.visit('carrinho')
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            )
+
+    });
+
+});
